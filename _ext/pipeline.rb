2f21dd5c7e6bae4ac81efbdb498f681b48468e80
Elliot Shank <github@galumph.com>
2013-07-23 16:36:13 -0700

    Remove stuff.

--- _ext/pipeline.rb
+++
@@ -1,9 +0,0 @@
-require 'zurb-foundation'
-require './stylesheets/_themes/lib/functions.rb'
-
-Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new '/news'
-  # extension Awestruct::Extensions::Indexifier.new
-  # Indexifier *must* come before Atomizer
-  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
-end
